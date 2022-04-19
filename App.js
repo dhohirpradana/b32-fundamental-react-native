2301315ebf8a0cb42d244b9afe7873482f666f0d
Jody-septiawan <jody.septiawan5@gmail.com>
2022-04-19 10:58:49 +0700

    update

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View } from 'react-native';
 
-//Import Screen
+import Map from './src/screens/map';
 
 export default function App() {
   return (
-    <View>
+    <View style={{ marginTop: 200 }}>
       <StatusBar />
-      {/* Use Component */}
+      <Map />
     </View>
   );
 }
--- src/screens/map.js
+++ src/screens/map.js
@@ -1,14 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View, Text } from 'react-native';
 
 export default function Map() {
   // Create Dummy Data With Array
+  const cars = ['BMW', 'Mercedes-Benz', 'Ducati', 'Lexus'];
 
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+      <Text> This is List On React with Map </Text>
+      {cars.map((item, idx) => (
+        <Text key={idx}>{item}</Text>
+      ))}
     </View>
   );
 }
